ecd256e20c7d967c05fa0bf31c5c34a9a3a3fce6
samuelkalocaiEDU <ps.kalocai.sam@student.sostp.cz>
2026-06-04 21:11:46 +0200

    warn added for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -2,25 +2,26 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		adapter: adapter({
-			fallback: undefined,
-			pages: 'build',
-			assets: 'build',
-			precompress: true,
-			handleMissingId: 'warn'
-		}),
-		alias: {
-			$components: 'src/lib/components',
-			$lib: 'src/lib'
-		}
-	},
-	vitePlugin: {
-		inspector: false
-	}
+    compilerOptions: {
+        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+    },
+    kit: {
+        // Závorky u adaptéru musí zůstat ÚPLNĚ PRÁZDNÉ, aby byl Vercel spoko
+        adapter: adapter(), 
+        
+        // Sem správně patří ignorování chybějících odkazů:
+        prerender: {
+            handleMissingId: 'warn'
+        },
+        
+        alias: {
+            $components: 'src/lib/components',
+            $lib: 'src/lib'
+        }
+    },
+    vitePlugin: {
+        inspector: false
+    }
 };
 
 export default config;
